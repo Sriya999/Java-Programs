7ec2033086f74a35a1adab6bfa2de2b5d2ba4eac
Sriya999 <poosaalasriya@gmail.com>
2025-01-30 10:46:45 +0530

    Update and rename LC_1108_defanging_an_IpAddress.java to defanging_an_IpAddress.java

--- defanging_an_IpAddress.java
+++ defanging_an_IpAddress.java
@@ -1,5 +1,6 @@
 class Solution {
     public String defangIPaddr(String address) {
+        //using replace function
         String result = "";
         address = address.replace(".", "[.]");
         return address;

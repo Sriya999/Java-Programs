566eccc8f30f62ba4b9d44a3cd36a685a1f59efc
Sriya999 <poosaalasriya@gmail.com>
2025-02-07 22:41:05 +0530

    Create pattern_diagonal.java

---
+++ pattern_diagonal.java
@@ -0,0 +1,27 @@
+
+class Main {
+    public static void main(String[] args) {
+        /*
+              *****
+             *****
+            *****
+           *****
+          ***** 
+
+        */
+    int n=5;
+    for(int i=0;i<n;i++){
+        
+            for(int k=0;k<n-i;k++){
+                
+            System.out.print(" ");
+        }
+         for(int k=0;k<n;k++){
+                
+            System.out.print("*");
+        }
+        
+        System.out.println();
+    }
+    }
+}

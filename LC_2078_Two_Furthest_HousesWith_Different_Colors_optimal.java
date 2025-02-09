392413cf93f3921607e5a36accc08e347121e037
Sriya999 <poosaalasriya@gmail.com>
2025-02-09 15:48:09 +0530

    Update and rename furthermost.java to LC_2078_Two_Furthest_HousesWith_Different_Colors_optimal.java

--- LC_2078_Two_Furthest_HousesWith_Different_Colors_optimal.java
+++ LC_2078_Two_Furthest_HousesWith_Different_Colors_optimal.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxDistance(int[] colors) {
         int n = colors.length;
         int maxdist = 0;
-       
+       //case 1:comparing last element with starting elements which is diffrent from last element
         for (int i = 0; i < n; i++) {
             if (colors[i] != colors[n - 1]) {
                 int dist = Math.abs(i - (n - 1));
@@ -13,6 +13,7 @@ class Solution {
             }
 
         }
+        //case2:checking from last of array if there is elemt=ent other than first element
         for (int i = n - 1; i >= 0; i--) {
             if (colors[0] != colors[i]) {
                 int dist = Math.abs(i);
